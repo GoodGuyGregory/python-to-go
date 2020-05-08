2191eb42a970367258a0fa933b39aca4b807b892
GoodGuyGregory <greg.witt625@gmail.com>
2020-05-08 19:40:08 -0400

    adds working version of salescalc in go

--- Basic/go/salecalc/main.go
+++ Basic/go/salecalc/main.go
@@ -2,33 +2,34 @@ package main
 
 import "fmt"
 
-var orignalprice float64
-var discount float64
-
 func main() {
 
+	originalprice := 0.0
+	discount := 0.0
+
 	fmt.Println("enter the price of the item")
-	_, err := fmt.Scan(&originalprice)
+	// Scanf allows for flloat reading
+	_, err := fmt.Scanf("%f", &originalprice)
+
+	if err != nil {
+		fmt.Printf("Invalid price: $s\n", err)
+		// return will stop the program which is convention in go
+		return
+	}
 
 	fmt.Println("enter the precentage off")
-	_, err = fmt.Scan(&discount)
+	_, err = fmt.Scanf("%f", &discount)
 
-	if err == nil {
-		fmt.Println("the price is", originalprice)
+	if err != nil {
+		fmt.Printf("Invalid discount: $s\n", err)
 		// var precentDiscount int
-	} else {
-		fmt.Println(err)
+		return
 	}
+	// declares and creates the variable
+	percentOff := discount / 100.0
+	adjustedPrice := originalprice * percentOff
+	salePrice := originalprice - adjustedPrice
 
-	if err == nil {
-		// Convert to precentageOff
-		var precentageoff = discount / 100
-		// Calculate adjustment
-		var adjustedprice = originalprice * precentageoff
-		var saleprice = orignalprice - adjustedprice
-		fmt.Println("the precentage off is", saleprice)
-	} else {
-		fmt.Println(err)
-	}
-
+	// %.2f formats the decimal points for the values
+	fmt.Printf("The percent off is %.2f.\n The Price will be: %.2f\n", percentOff, salePrice)
 }
